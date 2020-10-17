740ae8a782ddc969c82bb930214774db7f784c92
f1tzy <winndini@gmail.com>
2020-10-17 20:32:26 +0300

    add reg form

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,40 +1,40 @@
 import React from "react";
-import './LoginPage.css'
+import './LoginPage.css';
+import {NavLink} from 'react-router-dom';
 
 const LoginPage = () => {
   return (
     <div>
+      <NavLink className="btn btn-primary btn-back" to="/">На главную</NavLink>
       <div className="card auth-card" style={{width: '20rem'}}>
         <div className="card-header">
             <ul className="nav nav-tabs card-header-tabs">
                 <li className="nav-item">
-                    <a className="nav-link active" href="/login">Авторизация</a>
+                    <NavLink className="nav-link active" to="/login">Авторизация</NavLink>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/auth">Регистрация</a>
+                    <NavLink className="nav-link" to="/auth">Регистрация</NavLink>
                 </li>
             </ul>
         </div>
         <div className="card-body">
-          <h3 className="card-title text-center">Авторизация</h3>
+          <h4 className="card-title text-center">Авторизация</h4>
             <form className="popup__form" name="login">
                 <div className="form-group">
-                    <label className="" htmlFor="email">Email</label>
+                    {/* <label className="" htmlFor="email">Email</label> */}
                     <input type="email" name="email" className="form-control" placeholder="Введите почту" id="email"
                         pattern="^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,}$" required minLength="2" maxLength="30" />
                 </div>
-                <span className="error error-email"></span>
                 <div className="form-group">
-                    <label className="" htmlFor="password">Пароль</label>
+                    {/* <label className="" htmlFor="password">Пароль</label> */}
                     <input type="password" name="pass" className="form-control" placeholder="Введите пароль" required
                         minLength="8" id="password"/>
                 </div>
-                <span className="error error-pass"></span>
                 <div className="custom-control custom-checkbox mb-3">
-                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                    <label className="custom-control-label" htmlFor="customCheck1">Запомнить меня</label>
+                    <input type="checkbox" className="custom-control-input" id="custom-checkbox" />
+                    <label className="custom-control-label" htmlFor="custom-checkbox">Запомнить меня</label>
                 </div>
-                <button className="btn btn-primary btn-block">Войти</button>
+                <button type="submit" className="btn btn-primary btn-block btn-pill">Войти</button>
             </form>
         </div>
       </div>
