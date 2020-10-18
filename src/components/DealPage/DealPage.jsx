7c47be507c6f2aaed337257c7e551645a3e5e124
yvyletkov <yvyletkov@ya.ru>
2020-10-18 19:06:45 +0300

    deal page timeline upgrade

--- src/components/DealPage/DealPage.jsx
+++ src/components/DealPage/DealPage.jsx
@@ -3,23 +3,45 @@ import s from "./DealPage.module.css";
 import './DealPage.module.css'
 import {connect} from "react-redux";
 import {login} from "../../redux/reducer";
+import vvpImg from "../../img/vvp.jpg";
+import damImg from "../../img/dam.jpg";
 
-const StatusTimeline = ({status = 4}) => {
+const StatusTimeline = ({status = 4, withDelivery: delivery = true, problem = true}) => {
 
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let array = [];
+    let amount = !delivery && !problem ? 6 : (delivery && !problem) || (!delivery && problem) ? 7 : 8;
+    for (let i = 1; i <= amount; i++) {
+        array.push(i);
+    }
+
+    const descriptions = {
+        1: 'Сделка создана',
+        2: 'Приглашение второго участника',
+        3: 'Условия приняты, ожидается оплата',
+        4: 'Сделка выполняется',
+        5: 'Передано в доставку, ожидание приемки',
+        6: 'Приемка работ заказчиком',
+        7: 'Работы не приняты, передача в арбитраж',
+        8: 'Работы приняты, сделка закрыта',
+    }
 
     let statusItems = array.map((item) => {
-        return <div key={item} className={s.statusItemWrapper}>
-            <div
-                className={item < status ? `${s.statusItem} ${s.archieved} ${s.past}` : item == status ? `${s.statusItem} ${s.archieved}` : `${s.statusItem}`}>
-                {item < status ? <i className="fas fa-check fa-2x"></i> : null}
+        return <>
+            <div key={item} className={s.statusItemWrapper}>
+                <div
+                    className={item < status ? `${s.statusItem} ${s.archieved} ${s.past}` : item == status ? `${s.statusItem} ${s.archieved}` : `${s.statusItem}`}>
+                    {item < status ? <i className="fas fa-check fa-2x"></i> : null}
+                </div>
+                <div className={`${s.statusDescription} ${item % 2 === 0 ? s.top : s.bottom} ${item > status ? s.muted : null}`}>
+                    {descriptions[item]}
+                </div>
             </div>
-        </div>
-    })
+        </>
+    });
 
     return (
         <div className={s.statusTimelineWrapper}>
-            <h3><span className="badge badge-light">Ожидает оплаты</span></h3>
+            <h3><span style={{textTransform: 'uppercase'}} className="badge badge-outline-light">CТАТУС: {descriptions[status]}</span></h3>
             <div className={s.statusTimeline}>
                 {statusItems}
             </div>
@@ -38,25 +60,35 @@ const DealPage = () => {
                     <li className="breadcrumb-item active" aria-current="page">Просмотр сделок</li>
                 </ol>
             </nav>
-            <div className='row mt-5'>
+            <div className='row mt-4'>
                 <div className='col-md-9'>
                     <div className='card-body'>
-                        <h4>Сделка №9999</h4>
+                        <h3><span className="badge badge-light">Cделка №2135</span></h3>
                     </div>
-                    <div className='card mb-4'>
+                    <div className='mb-5'>
                         <div className='card-body'>
 
                             <StatusTimeline/>
 
                         </div>
                     </div>
-                    <div className='mb-2'>
-                        <div className='card-body'>
-                            <h4 className='card-title'>Предмет сделки</h4>
-                            <p><span className="badge badge-light">Что-то продаем</span></p>
+
+                    <div className='row'>
+                        <div style={{borderRight: "1px solid #c9c9c9"}} className='col-6'>
+                            <div className='card-body'>
+                                <h4 className='card-title'>Предмет сделки</h4>
+                                <p>Моющий пылесос с аква фильтром</p>
+                            </div>
+                        </div>
+                        <div className='col-6'>
+                            <div className='card-body'>
+                                <h4 className='card-title'>Файлы сделки</h4>
+                                <p>К этой сделке файлы не прикреплены</p>
+                            </div>
                         </div>
                     </div>
-                    <div className='mb-4'>
+
+                    <div className='mb-1'>
                         <div className='card-body'>
                             <h4 className='card-title'>История сделки</h4>
                             <p>
@@ -71,7 +103,8 @@ const DealPage = () => {
                                 принятия условий второго участника<br/>
                                 <span style={{color: "#fff"}} className="badge badge-warning mr-2 mb-2">07.10.2020 23:26:49</span> Условия
                                 сделки приняты, ожидание оплаты от заказчика<br/>
-                                <span style={{color: "#fff"}} className="badge badge-warning mr-2 mb-2">07.10.2020 23:27:35</span> Выбран тип
+                                <span style={{color: "#fff"}} className="badge badge-warning mr-2 mb-2">07.10.2020 23:27:35</span> Выбран
+                                тип
                                 оплаты. Физ.лицо. Проведение электронного платежа<br/>
                                 <span style={{color: "#fff"}} className="badge badge-warning mr-2 mb-2">07.10.2020 23:28:42</span> Оплата
                                 поступила, исполнитель приступил к выполнению работ<br/>
@@ -80,12 +113,7 @@ const DealPage = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='card mb-5'>
-                        <div className='card-body'>
-                            <h4 className='card-title'>Файлы сделки</h4>
-                            <p>К этой сделке файлы не прикреплены</p>
-                        </div>
-                    </div>
+
                     <div className='mb-4'>
                         <div className='card-body'>
                             <h4 className='card-title mb-3'>Чат между участниками сделки</h4>
@@ -95,13 +123,14 @@ const DealPage = () => {
                                     {/*<p className='text-muted mb-2 mt-2'>Пока сообщений нет</p>*/}
 
                                     <div className='mb-1'>
-                                        <h5 className='d-inline'><span className="badge badge-secondary mr-2 mb-2">Владимир (07.10.2020 23:27:35)</span>
+                                        <h5 className='d-inline'><span
+                                            className="badge badge-outline-secondary mr-2 mb-2">Владимир (07.10.2020 23:27:35)</span>
                                         </h5>
                                         <span>Привет, как там с деньгами</span>
                                     </div>
 
                                     <div className='mb-1'>
-                                        <h5 className='d-inline'><span className="badge badge-light mr-2 mb-2">Дмитрий (07.10.2020 23:28:15)</span>
+                                        <h5 className='d-inline'><span className="badge badge-outline-dark mr-2 mb-2">Дмитрий (07.10.2020 23:28:15)</span>
                                         </h5>
                                         <span>Ты кому звонишь?</span>
                                     </div>
@@ -128,14 +157,24 @@ const DealPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className=' col-md-3'>
-                    <div className='card-body'>
-                        <h3><span className="badge badge-secondary">Продавец</span></h3>
-                        <p>Информация о продавце</p>
+                <div className='col-md-3'>
+                    <div className={`card ${s.sidebarCard}`}>
+                        <div className='card-body'>
+                            <img className={s.cardImg} src={vvpImg} alt="Путин"/>
+                            <h4><span className="badge badge-pill badge-secondary">Продавец</span></h4>
+                            <div>Путин</div>
+                            <div>Владимир</div>
+                            <div>email: vvp@scrows.ru</div>
+                        </div>
                     </div>
-                    <div className='card-body'>
-                        <h3><span className="badge badge-secondary">Покупатель</span></h3>
-                        <p>Информация о покупателе</p>
+                    <div className={`card ${s.sidebarCard}`}>
+                        <div className='card-body'>
+                            <img className={s.cardImg} src={damImg} alt="Медведев"/>
+                            <h4><span className="badge badge-pill badge-secondary">Покупатель</span></h4>
+                            <div>Медведев</div>
+                            <div>Дмитрий</div>
+                            <div>email: dam@scrows.ru</div>
+                        </div>
                     </div>
                 </div>
             </div>
