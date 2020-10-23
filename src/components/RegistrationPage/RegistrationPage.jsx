4c8b6fd98c12256d7c0b00dbf506393907b98476
f1tzy <winndini@gmail.com>
2020-10-23 10:33:13 +0300

    add redux-forms and fix validation

--- src/components/RegistrationPage/RegistrationPage.jsx
+++ src/components/RegistrationPage/RegistrationPage.jsx
@@ -1,208 +1,92 @@
-import React, { Component } from "react";
+import React from "react";
 import "./RegistrationPage.css";
 import { NavLink } from "react-router-dom";
-import ErrorField from "../shared/ErrorFiled/ErrorField";
+import { Field, reduxForm } from "redux-form";
+import { validate, warn } from "../../utils/validators/validators";
+import {
+  renderCheckBox,
+  renderInput,
+} from "../shared/FormContols/FormControls";
 
-class RegistrationPage extends Component {
-  state = {
-    email: "",
-    password: "",
-    passwordRepeat: "",
-    formErrors: { email: "", password: "", passwordRepeat: "" },
-    emailValid: false,
-    passwordValid: false,
-    passwordMatch:false,
-    formValid: false,
-    onFocusEmail: false,
-    onFocusPass: false,
-    onFocusPassRepeat:false,
-  };
-
-  handleUserInput(e) {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({ [name]: value }, () => {
-      this.validateField(name, value)
-      this.focusInput(name);
-    });
-  }
-
-  validateField(fieldName, value) {
-    let fieldValidationErrors = this.state.formErrors;
-    let emailValid = this.state.emailValid;
-    let passwordValid = this.state.passwordValid;
-    let passwordRepeat = this.state.passwordRepeat;
-    let password = this.state.password;
-    let passwordMatch = this.state.passwordMatch;
-    switch (fieldName) {
-      case "email":
-        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-        fieldValidationErrors.email = emailValid ? "" : "email введен не верно";
-        break;
-      case "password":
-        passwordMatch = password === passwordRepeat && passwordRepeat.length !== 0;
-        fieldValidationErrors.passwordRepeat = password === passwordRepeat ? "" : "пароли не совпадают";
-        passwordValid = value.length >= 6;
-        fieldValidationErrors.password = passwordValid
-          ? ""
-          : "пароль слишком короткий";
-        break;
-      case "passwordRepeat":
-        passwordMatch = password === passwordRepeat && passwordRepeat.length !== 0;
-        fieldValidationErrors.passwordRepeat = password === passwordRepeat ? "" : "пароли не совпадают";
-        break;
-      default:
-        break;
-    }
-    this.setState(
-      {
-        formErrors: fieldValidationErrors,
-        emailValid: emailValid,
-        passwordValid: passwordValid,
-        passwordMatch:passwordMatch,
-      },
-      this.validateForm
-    );
-  }
-
-  validateForm() {
-    this.setState({
-      formValid: this.state.emailValid && this.state.passwordValid && this.state.passwordMatch,
-    });
-  }
-
-  focusInput(name) {
-    switch (name) {
-      case "email":
-        this.setState({
-          onFocusEmail: true,
-        });
-        break;
-      case "password":
-        this.setState({
-          onFocusPass: true,
-        });
-        break;
-      case "passwordRepeat":
-        this.setState({
-          onFocusPassRepeat:true,
-        });
-        break;
-      default:
-        break;
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {/* <NavLink className="btn btn-primary btn-back" to="/">На главную</NavLink> */}
-        <div className="card auth-card" style={{ width: "20rem" }}>
-          <div className="card-header">
-            <ul className="nav nav-tabs card-header-tabs">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/login">
-                  Авторизация
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link active" to="/auth">
-                  Регистрация
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          <div className="card-body">
-            <h4 className="card-title text-center">Регистрация</h4>
-            <form className="popup__form" name="login">
-              <div className="form-group">
-                <input
-                  type="email"
-                  name="email"
-                  className={
-                    this.state.onFocusEmail
-                      ? this.state.emailValid
-                        ? "form-control is-valid"
-                        : "form-control is-invalid"
-                      : "form-control"
-                  }
-                  placeholder="Введите почту"
-                  value={this.state.email}
-                  pattern="^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,}$"
-                  required
-                  minLength="2"
-                  maxLength="30"
-                  onChange={this.handleUserInput.bind(this)}
-                  onFocus={this.handleUserInput.bind(this)}
-                />
-                <ErrorField message={this.state.formErrors.email} />
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  name="password"
-                  className={
-                    this.state.onFocusPass
-                      ? this.state.passwordValid
-                        ? "form-control is-valid"
-                        : "form-control is-invalid"
-                      : "form-control"
-                  }
-                  placeholder="Придумайте пароль"
-                  required
-                  value={this.state.password}
-                  minLength="6"
-                  onChange={this.handleUserInput.bind(this)}
-                  onFocus={this.handleUserInput.bind(this)}
-                />
-                <ErrorField message={this.state.formErrors.password} />
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  name="passwordRepeat"
-                  className={
-                    this.state.onFocusPassRepeat
-                      ? this.state.passwordMatch
-                        ? "form-control is-valid"
-                        : "form-control is-invalid"
-                      : "form-control"
-                  }
-                  placeholder="Повторите пароль"
-                  required
-                  value={this.state.passwordRepeat}
-                  minLength="6"
-                  onChange={this.handleUserInput.bind(this)}
-                  onFocus={this.handleUserInput.bind(this)}
-                />
-                <ErrorField message={this.state.formErrors.passwordRepeat} />
-              </div>
-              <div className="custom-control custom-checkbox mb-3">
-                <input
-                  type="checkbox"
-                  className="custom-control-input"
-                  id="custom-checkbox"
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor="custom-checkbox"
-                >
-                  Я принимаю условия Пользовательского соглашения
-                </label>
-              </div>
-              <button
-                type="submit"
-                className="btn btn-primary btn-block btn-pill"
-                disabled={!this.state.formValid}
-              >
-                Войти
-              </button>
-            </form>
-          </div>
-        </div>
+const AuthForm = (props) => {
+  const { handleSubmit, pristine, reset, submitting } = props;
+  return (
+    <form className="popup__form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <Field
+          placeholder="Введите почту"
+          name="email"
+          type="email"
+          component={renderInput}
+        />
       </div>
-    );
-  }
-}
+      <div className="form-group">
+        <Field
+          placeholder="Придумайте пароль"
+          name="passwordFirst"
+          type="password"
+          component={renderInput}
+          label="Email"
+        />
+      </div>
+      <div className="form-group">
+        <Field
+          placeholder="Повторите пароль"
+          name="passwordSecond"
+          type="password"
+          component={renderInput}
+          label="Email"
+        />
+      </div>
+      <div className="custom-control custom-checkbox mb-3">
+        <Field
+          type="checkbox"
+          name="onChecked"
+          component={renderCheckBox}
+          label={"Я принимаю условия Пользовательского соглашения"}
+        />
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="btn btn-primary btn-block btn-pill"
+          disabled={submitting || pristine}
+        >
+          Войти
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const AuthReduxForm = reduxForm({ form: "auth", validate, warn })(AuthForm);
+
+const RegistrationPage = (props) => {
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="card auth-card" style={{ width: "20rem" }}>
+      <div className="card-header">
+        <ul className="nav nav-tabs card-header-tabs">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/login">
+              Авторизация
+            </NavLink>
+          </li>
+          <li className="nav-item active">
+            <NavLink className="nav-link" to="/auth">
+              Регистрация
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="card-body">
+        <h4 className="card-title text-center">Авторизация</h4>
+        <AuthReduxForm onSubmit={handleSubmit} />
+      </div>
+    </div>
+  );
+};
 
 export default RegistrationPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
 import reducer from "./reducer";
+import { reducer as formReducer } from "redux-form";
 
+// const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+const reducers = combineReducers({
+  form: formReducer,
+  main: reducer,
+});
 
-const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
