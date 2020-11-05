58287fbf03741afb4e584858d31b7aa8526107af
f1tzy <winndini@gmail.com>
2020-11-05 23:52:48 +0300

    add form credit card

--- src/components/shared/FormContols/FormControls.jsx
+++ src/components/shared/FormContols/FormControls.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ErrorField from "../ErrorFiled/ErrorField";
+import s from './FormControls.module.css'
 
 const renderInput = ({
   input,
@@ -59,4 +60,27 @@ const renderPersonalAreaInput = ({
   </>
 );
 
-export { renderCheckBox, renderInput, renderPersonalAreaInput };
+const renderCardNumberInput = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning },
+  placeholder,
+}) => (
+  <>
+    <input
+      className={
+        !touched ? `form-control ${s.cardNumberInput}` : `form-control is-invalid  ${s.cardNumberInput}`
+       && (!error ? `form-control is-valid  ${s.cardNumberInput}` : `form-control is-invalid ${s.cardNumberInput}`)
+    }
+      {...input}
+      placeholder={placeholder}
+      type={type}
+    />
+    {touched &&
+      ((error && <ErrorField message={error} />) ||
+        (warning && <ErrorField message={warning} />))}
+  </>
+);
+
+export { renderCheckBox, renderInput, renderPersonalAreaInput, renderCardNumberInput };
