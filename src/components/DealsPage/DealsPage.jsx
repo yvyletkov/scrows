3efb2d31f16b90059623cafcd984eb3a4d43e5b3
f1tzy <winndini@gmail.com>
2020-11-21 15:47:18 +0300

    add deals list

--- src/components/DealsPage/DealsPage.jsx
+++ src/components/DealsPage/DealsPage.jsx
@@ -6,27 +6,41 @@ import {getDealsData} from "../../redux/DealsPageReducer";
 
 const DealsPage = (props) => {
 
-    console.log(props)
+    console.log(props);
 
     const {getDealsData, deals} = props;
     useEffect(() => {
         getDealsData()
     }, []);
 
-    const dealsArray = deals.map((deal) => {
+    const dealsList = deals.map((deal) => {
         let month = new Date(deal.created_at).getUTCMonth() + 1;
         let day = new Date(deal.created_at).getUTCDate();
         let year = new Date(deal.created_at).getUTCFullYear();
 
         let newDate = `${day}.${month}.${year}`;
         return (
-            <tr className={s.itemDeals} key={deal.id}>
-                <th className={s.table4}>{deal.id}</th>
-                <td className={s.table24}>{newDate}</td>
-                <td className={s.table24}>{deal.subject}</td>
-                <td className={s.table24}>{deal.price} &#8381;</td>
-                <td className={s.table24}>{deal.status.title}</td>
-            </tr>
+            <div className={`card mt-3 ${s.card}`} key={deal.id}>
+                <div className="card-header d-flex flex-lg-row flex-column p-0">
+                    <p className={s.nameDeal}>{deal.subject}</p>
+                </div>
+                <div className={`card-body ${s.cardBody}`}>
+                    <div className="d-lg-flex d-block">
+                        <div className={`col-lg-4 col-12 ${s.block}`}>
+                            <p className={s.titleBlock}>Дата создания</p>
+                            <p className={s.contentBlock}>{newDate}</p>
+                        </div>
+                        <div className={`col-lg-4 col-12 ${s.block}`}>
+                            <p className={s.titleBlock}>Сумма сделки</p>
+                            <p className={s.contentBlock}>{deal.price} &#8381;</p>
+                        </div>
+                        <div className={`col-lg-4 col-12 ${s.block}`}>
+                            <p className={s.titleBlock}>Статус</p>
+                            <span className={`badge badge-info ${s.contentBlock}`}>{deal.status.title}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     })
 
@@ -34,23 +48,10 @@ const DealsPage = (props) => {
         <div className="container mt-5">
             <div className="row">
                 <DealsTabs/>
-                <div className="card col-8 p-0">
+                <div className="card col-lg-8 p-0 col-12">
                     <h4 className="card-header">Сделки</h4>
                     <div className="card-body pt-0">
-                        <table className="table table-hover table-borderless">
-                            <thead>
-                            <tr>
-                                <th className={s.table4}>№</th>
-                                <th className={s.table24}>Дата создания</th>
-                                <th className={s.table24}>Предмет</th>
-                                <th className={s.table24}>СТоимость</th>
-                                <th className={s.table24}>Статус</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {dealsArray}
-                            </tbody>
-                        </table>
+                        {dealsList}
                     </div>
                 </div>
             </div>
